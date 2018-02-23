9b5f9aa514cafbf640f4f058c75b4dedc7845b96
blaise594 <danielbrogers@gmail.com>
2018-02-23 17:54:01 -0500

    first commit

---
+++ MenuConsole/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuConsole
+{
+    class MenuItem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+
+
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+
+        public MenuItem(
+            string Name, 
+            string Desription, 
+            double Price, 
+            string Category)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Price = Price;
+            this.Category = Category;
+            this.DateAdded = DateAdded;
+        }
+        
+    }
+}
---
+++ MenuConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MenuConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
